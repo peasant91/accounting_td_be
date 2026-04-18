08f648fba9cce569b37d932185fb353b9f4b8d3a
Kevin Raymond <kevin@Kevins-MacBook-Pro.local>
2026-04-19 07:26:05 +0800

    feat(billing): add base_currency config key

--- config/billing.php
+++ config/billing.php
@@ -2,6 +2,7 @@
 
 return [
     'default_currency' => env('BILLING_DEFAULT_CURRENCY', 'IDR'),
+    'base_currency' => env('BILLING_BASE_CURRENCY', 'IDR'),
 
     'invoice_number' => [
         'prefix' => env('BILLING_INVOICE_PREFIX', 'INV'),
